756658093e2af3ef153ecfa2b315e09f0bb4d958
nikhil1610 <54029364+nikhil1610@users.noreply.github.com>
2021-06-12 18:55:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,40 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+    const [input, setInput] = useState("github") ;
+  return (
+    <React.Fragment>
+    <link rel="stylesheet"  
+     href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    ></link>
+  <div className="container" style={{backgroundColor:"rgb(0,0,0)"}}>   
+    <div className="input mt-5">
+      <label>Enter any value:</label>
+      <span>
+        <input type="text" placeholder="Enter anything..." value={ input } onChange={(e)=>{setInput(e.target.value)}}/>
+      </span>
+    </div> 
+    <div className="default-avatar container mt-3">
+      <div className="identicon" style={{float:"left"}}>
+        <Identicon string={input} size={200}/>
+      </div>
+      <div className="robohash" style={{float:"right"}}>
+        {/* {console.log(input)} */}
+        <img src={`https://robohash.org/${input}.png`}
+         style={{height:"200px", width: "200px"}}
+        />
+
+      </div>
+
+    </div>
+  </div>
+  </React.Fragment>
+  );
+
 );
 
 // If you want to start measuring performance in your app, pass a function
