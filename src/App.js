2def2c367ad7aab699d0d1673d0103419b751758
nikhil1610 <nikhilgautam106@gmail.com>
2021-06-14 19:15:19 +0530

    added caption

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import Identicon from 'react-identicons';
 import {Button, Form} from 'semantic-ui-react';
-import styles from './App.css';
+import './App.css';
 
 function App() {
   const [input, setInput] = useState("") ;
@@ -15,7 +15,6 @@ function App() {
       <Form>
       <label>Enter any value</label>
         <Form.Field inline>
-          {console.log(styles)}
         <input className="inputText" type="text" placeholder="Enter anything..." value={ input } onChange={(e)=>{setInput(e.target.value)}} />
         <Button  color="green" onClick={()=>setVal(input)}>Generate </Button> 
         </Form.Field>
@@ -23,13 +22,19 @@ function App() {
     </div> 
     <div className="default-avatar container" style={{marginTop:"50px", height:"fit-content"}}>
       <div className="identicon" style={{float:"left", transform: "translate(20%, 2%)"}} >
+        <div style={{display:"inline-grid"}}>
         <Identicon string={val} size={200}/>
+        <text className="caption">Identicon</text>
+        </div>
       </div>
       <div className="robohash" style={{float:"right", transform: "translate(-25%, 2%)"}}>
         {/* {console.log(input)} */}
+        <div style={{display:"inline-grid"}}>
         <img src={`https://robohash.org/${val}.png`}
          alt="robohash" style={{height:"200px", width: "200px"}}
         />
+        <text className="caption">Robohash</text>
+        </div>
       </div>
 
     </div>
