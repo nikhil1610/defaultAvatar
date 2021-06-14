b58c2fe8b4944ab9e17811986b55cb3bb7128426
nikhil1610 <nikhilgautam106@gmail.com>
2021-06-14 21:37:14 +0530

    navbar modified

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import App from './App';
 ReactDOM.render(
     <React.StrictMode>
         <header className="header pt-2" style={{backgroundColor:"rgb(36, 41, 46)", height:"15vh"}}>
-            <div className="mt-2" style={{hieght:"50px"}}>
+            <div className="mt-2" style={{display:"flex"}}>
                 <h3 className="ml-5" style={{color:"blueviolet", width:"200px"}}>User Avatar</h3>
-                <a href="https://github.com/nikhil1610/defaultAvatar" target="/blank" className="mr-5" style={{textDecoration:"none", float:"right", transform:"translate(5%, -100%)"}}>
+                <a href="https://github.com/nikhil1610/defaultAvatar" target="/blank" className="mr-5" 
+                   style={{textDecoration:"none", marginLeft:"auto", transform:"translate(10px, 10px)"}}>
                     Github
                 </a>
             </div>
